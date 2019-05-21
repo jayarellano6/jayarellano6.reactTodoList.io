703af32e0b268d5775c9e24855a148591849e624
Jay Arellano <jaarellano@csumb.edu>
2019-05-20 19:12:45 -0700

    remove /build

---
+++ build/precache-manifest.5affd46de0ee9aed8c8550cdaf2a9fed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "518b92e14885c1ebb0738bd5c5c1bfab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ace380990ff6adabbeda",
+    "url": "/static/css/main.090a3dbb.chunk.css"
+  },
+  {
+    "revision": "5179b875ed11e30f1a3a",
+    "url": "/static/js/2.cb6e83f2.chunk.js"
+  },
+  {
+    "revision": "ace380990ff6adabbeda",
+    "url": "/static/js/main.aa3fa36a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
